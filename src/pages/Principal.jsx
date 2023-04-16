862d8675eb774b811d5ca58592eca35db50ce82f
Robertinho <102814124+jair000@users.noreply.github.com>
2023-04-16 07:59:46 -0500

    nose q hice

--- src/pages/Principal.jsx
+++ src/pages/Principal.jsx
@@ -8,12 +8,19 @@ import '../App.css'
 export function Principal() {
   return (
     <div className="App">
+      
       <div className='registro'>
-        <div className='header'><img src="/logoo.png" alt="header" /></div>
-        <div className="reserva"><img src="/reserva.png" alt="reserva" /></div>
+        <div className='header'>
+          <img src="/logoo.png" alt="header" />
+          </div>
+        <div className="reserva">
+          <img src="/reserva.png" alt="reserva" />
+          </div>
         <a href='#' className="btn-fixedwsp">Vía Whatsapp <FontAwesomeIcon icon={faWhatsapp} /> </a>
         <Link to={"/registro-virtual"} className="btn-fixedweb">Vía Online <FontAwesomeIcon icon={faMagnifyingGlass} /></Link>
       </div>
+
+
       <div className="cargos">
         <h3>Contamos con un grupo de expertos profesionales de la salud a tu disposición!</h3>
       </div>
--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -7,32 +7,50 @@ import { Link } from 'react-router-dom';
 export function Registro() {
   return (
     <div className="Register">
+
       <div className="header-registro">
         <div className="logo">
-          <img src="/logoo.png" alt="header" /></div>
+          <img src="/logoo.png" alt="header" />
+          </div>
         <div className="sac">
           <Link to={"#"} className='sac'>
           <FontAwesomeIcon icon={faHeadset} />
           </Link>
         </div>
       </div>
+
+
+
       <div className="Container">
         <h1>Ingresa y separa tu cita virtual</h1>
         <p>Bienvenido, ingrese sus datos para poder <br /> reservar su cita:</p>
 
-        <form action="">
-          <label htmlFor="">Ingrese su dni <br /></label>
-          <input type="text" maxLength={8}/> <br />
-          <label htmlFor="">Ingrese su contraseña  <br /></label>
-          <input type="password" minLength={6}/> <br />
+        <form>
+
+          <input type="text" maxLength={8} required/> <br />
+          
+          <label className='lbl-dni'>
+            <span className='txt-dni'>Ingrese su dni </span>
+          </label>
+          
+          <br />
+
+          <label className='lbl-pass'>Ingrese su contraseña</label> <br />
+          <input type="password" minLength={6} required/>
+          <br />
+
           <div className='ingreso-flex'>
           <button className='btn-ingresar'>Ingresar</button>
           <Link to={"#"}>Recupera Contraseña</Link>
           </div>
         </form>
+
+
           <div className="registro-flex">
             <p>¿No tienes cuenta?</p>
-            <div className='btn-register'><Link to={"#"} className='btn-re'>Registrate</Link></div>
+            <div className='btn-register'>
+              <Link to={"#"} className='btn-re'>Registrate</Link>
+              </div>
           </div>
           
       </div>
